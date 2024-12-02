070fa8fa1914b0c19e66cbc95857c22e6b9ccc85
Gustavo Zambonin <gzmbnn@gmail.com>
2024-12-02 02:41:33 -0300

    fixed gray order unranking and added enup order

--- src/unrank.c
+++ src/unrank.c
@@ -27,6 +27,7 @@ typedef void (*unrank_func)(uint16_t *, const uint16_t, const uint16_t,
 void lex_it(uint16_t *rop, uint16_t n, uint16_t k, uint16_t d, uintx rank);
 void lex_plus_it(uint16_t *rop, uint16_t n, uint16_t k, uint16_t d, uintx rank);
 void gray_it(uint16_t *rop, uint16_t n, uint16_t k, uint16_t d, uintx rank);
+void enup_it(uint16_t *rop, uint16_t n, uint16_t k, uint16_t d, uintx rank);
 
 static int cache_type = NO_CACHE;
 static uint16_t param_k = 0;
@@ -132,7 +133,7 @@ void build_bin_cache(const uint16_t n, const uint16_t k, const uint16_t d) {
   bin_cache = calloc((uint32_t)((n + k + 1) * param_k), sizeof(uintx));
   assert(bin_cache != NULL);
 
-  for (uint32_t row = 0; row < n + k + 1; ++row) {
+  for (uint32_t row = 0; row < (uint32_t) (n + k + 1); ++row) {
     bin_cache[0 + k * row] = 1;
     bin_cache[1 + k * row] = row;
 
@@ -183,7 +184,7 @@ void lex_plus_it(uint16_t *rop, const uint16_t n, const uint16_t k,
     uintx left = 0;
     intx right = inner_bic_with_sums(it_n, i, d, prev_sum);
 
-    for (part = 0; part < min(it_n, d) && rank >= right; ++part) {
+    for (part = 0; part < min(it_n, d) && rank >= (uintx) right; ++part) {
       left = right;
       uint16_t numerator = (it_n - part);
       uint16_t denominator = (it_n - part) + i - 1;
@@ -218,7 +219,27 @@ void gray_it(uint16_t *rop, const uint16_t n, const uint16_t k,
          ++part, rank -= count) {
     }
     if (part & 1U) {
-      rank -= bic(it_n - part, i - 1, d) - 1;
+      rank = count - 1 - rank;
+    }
+  }
+
+  rop[0] = it_n;
+}
+
+void enup_it(uint16_t *rop, const uint16_t n, const uint16_t k,
+             const uint16_t d, uintx rank) {
+  uint16_t it_n = n;
+  uint16_t part = 0;
+  uintx count = 0;
+
+  rank = bic(it_n, k, d) - 1 - rank;
+  for (uint16_t i = k - 1; i > 0; rop[i] = part, --i, it_n -= part) {
+    for (part = 0;
+         count = bic(it_n - part, i, d), part < min(it_n, d) && rank >= count;
+         ++part, rank -= count) {
+    }
+    if (!(part & 1U)) {
+      rank = count - 1 - rank;
     }
   }
 
@@ -260,6 +281,8 @@ int32_t parse_args(int32_t argc, char **argv, uint16_t *n, uint16_t *k,
         *unrank = lex_plus_it;
       } else if (strcmp(optarg, "gray") == 0) {
         *unrank = gray_it;
+      } else if (strcmp(optarg, "enup") == 0) {
+        *unrank = enup_it;
       } else if (fprintf(stderr, "Invalid parameter.\n")) {
         return 1;
       }
@@ -304,7 +327,8 @@ int32_t parse_args(int32_t argc, char **argv, uint16_t *n, uint16_t *k,
         "         Available options are:\n"
         "           * `lex` (lexicographic order);\n"
         "           * `lexplus` (lexicographic order reusing partial sums);\n"
-        "           * `gray` (minimal change order, or Gray code).\n"
+        "           * `gray` (minimal change order, or Gray code);\n"
+        "           * `enup` (even numbers up, odd numbers down).\n"
         "\n"
         "  -i, --iterations=<uint64_t>\n"
         "         Number to repeatedly unrank random integers.\n"
