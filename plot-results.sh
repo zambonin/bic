098b631e7f9b768965a022d6ff0cfa2b136d3665
Gustavo Zambonin <gzmbnn@gmail.com>
2025-01-23 02:36:43 -0300

    organize scripts to plot unranking performance

--- plot-results.sh
+++ plot-results.sh
@@ -2,37 +2,47 @@
 
 IT="${1:-$(make -pqrR | awk '/^IT/ { print $NF }')}"
 
-for LEVEL in 256 ; do
-  RAW_DATA_PATH="$TMPDIR/bitint-time-all-versions-it-$IT-m-$LEVEL.dat"
-  rm -f $TMPDIR/bitint-test-*.dat "$RAW_DATA_PATH"
+SECURITY="256 512"
+ALGORITHMS="colex colexpart emk enup"
+CACHE_STRAT="none bin comb"
 
-  for ALG in colex enup emk ; do
-    for CACHE in none bin comb ; do
-    (
-      echo "$ALG-$CACHE"
-      make --silent IT=$IT ALG=$ALG CACHE=$CACHE clean test-$LEVEL
-    ) >| "$TMPDIR/bitint-test-$LEVEL-$ALG-$CACHE.dat"
+for LEVEL in $SECURITY ; do
+  ALL_DATA_PATH="$TMPDIR/bitint-cycles-all-m-$LEVEL-it-$IT.dat"
+  rm -f $TMPDIR/bitint-cycles-test-m-$LEVEL-*.dat "$ALL_DATA_PATH"
+
+  for ALG in $ALGORITHMS ; do
+    for CACHE in $CACHE_STRAT ; do
+      RAW_DATA_PATH="$TMPDIR/bitint-cycles-test-m-$LEVEL-it-$IT-a-$ALG-c-$CACHE.dat"
+      echo "$RAW_DATA_PATH"
+      (
+        echo "$ALG-$CACHE"
+        make --silent IT="$IT" ALG="$ALG" CACHE="$CACHE" \
+          clean "$IMPL" "test-$LEVEL"
+      ) >| "$RAW_DATA_PATH"
     done
   done
 
-  paste -d" " $TMPDIR/bitint-test-$LEVEL-*.dat \
+  paste -d" " $TMPDIR/bitint-cycles-test-m-$LEVEL-*.dat \
     | awk '
         NR == 1 { print "k", $0 }
-        NR > 1 {
-          printf "%3d ", $6
-          for (i = 19; i <= NF; i += 24) {
-            printf "%14.2lf", $i
+        NR != 1 {
+          printf "%6d", int($6)
+          for (i = 24; i <= NF; i += 24) {
+            printf "%14.2Lf", $i
           }
           printf "\n"
         }' \
     | column -t \
-    > "$RAW_DATA_PATH"
+    > "$ALL_DATA_PATH"
+
   gnuplot -e "
-    set linetype cycle 30;
+    stats '/dev/stdin' skip 1 nooutput;
+    max_col = STATS_columns;
+    set linetype cycle max_col;
     set key autotitle columnhead;
     set logscale y;
     set terminal png size 2560, 1440;
-    plot for [i = 2:10] '/dev/stdin' using 1:i
+    plot for [i = 2:max_col] '/dev/stdin' using 1:i
       with lines linewidth 3 smooth mcsplines
-  " < "$RAW_DATA_PATH" > "${RAW_DATA_PATH/dat/png}"
+  " < "$ALL_DATA_PATH" > "${ALL_DATA_PATH/dat/png}"
 done
